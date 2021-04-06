805d479d89d8fc115e3d41c0f7c61cb96dd246ae
Richie2810 <richiegoneill@gmail.com>
2021-04-06 20:12:12 +0200

    added a store for plans, made an action to get all plans for a single user

--- src/pages/Schedule/Schedule.js
+++ src/pages/Schedule/Schedule.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Jumbotron } from 'react-bootstrap'
-
+import { useDispatch } from 'react-redux'
+import { getUsersPlans } from '../../store/plans/actions'
 
 export default function Schedule() {
+const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getUsersPlans())
+    },[dispatch])
+
     return (
         <div>
             <Jumbotron>
---
+++ src/store/plans/actions.js
@@ -0,0 +1,39 @@
+import { apiUrl } from "../../config/constants";
+import axios from "axios";
+import { selectToken } from "./selectors";
+import {
+  appLoading,
+  appDoneLoading,
+  showMessageWithTimeout,
+  setMessage
+} from "../appState/actions";
+import { selectUser } from "../user/selectors";
+
+const userPlansFetched = plans => ({
+    type: 'plans/fetched',
+    payload :plans
+})
+
+export const getUsersPlans = () => async (dispatch, getState) => {
+    dispatch(appLoading())
+    const { token } = selectUser(getState())
+    try {
+        const response = await axios.get(`${apiUrl}/plans`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        console.log(response.data)
+        dispatch(appDoneLoading())
+    } catch(error) {
+        if (error.response) {
+          console.log(error.response.data.message);
+          dispatch(setMessage("danger", true, error.response.data.message));
+        } else {
+          console.log(error.message);
+          dispatch(setMessage("danger", true, error.message));
+        }
+        dispatch(appDoneLoading());
+      }
+}
