554d7fec523125890e5d4ca09269978109885409
Richie2810 <richiegoneill@gmail.com>
2021-04-06 21:56:22 +0200

    fixed merging branches errors

--- src/pages/Schedule/Schedule.js
+++ src/pages/Schedule/Schedule.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react'
 import { Jumbotron } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectUsersPlans } from '../../store/plans/selectors'
+import { getUsersPlans } from '../../store/plans/actions'
+import ActSchedule from '../../components/ActSchedule/ActSchedule'
 
 export default function Schedule() {
 const dispatch = useDispatch()
